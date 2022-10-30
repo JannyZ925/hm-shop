5e9b803b954aecf99aeefd2ba1ec6720621d8c86
张姣妮 <JannyZ925@163.com>
2022-10-30 20:57:08 +0800

    完成了轮播图

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+// 将按需引入的http挂载到顶级对象uni身上，页面可以通过uni.$http来发起请求
+uni.$http = $http
+$http.baseUrl = "https://www.uinav.com"
+// 请求开始前做一些事情(options表示请求的一些配置)
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title: "玩命加载中..."
+  })
+}
+// 请求结束后做一些事情
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
+// 封装请求失败的提示方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
@@ -11,15 +33,3 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,47 @@
 <template>
   <view>
-    Home
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, index) in swiperList" :key="item.goods_id">
+        <navigator class="swiper-item" :url="'../../subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
 <script>
+  import {
+    $http
+  } from '@escook/request-miniprogram';
   export default {
     data() {
       return {
-        
+        swiperList: []
       };
+    },
+    methods: {
+      // 获取轮播图数据
+      async getSwiperList() {
+        const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.swiperList = res.message
+      }
+    },
+    onLoad() {
+      this.getSwiperList()
     }
   }
 </script>
 
 <style lang="less">
+  swiper {
+    height: 330rpx;
 
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
